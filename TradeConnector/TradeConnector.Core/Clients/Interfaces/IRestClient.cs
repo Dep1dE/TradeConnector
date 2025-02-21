8be74b1444459708ed951ca133db7ee0c6997ec8
Stepanov_Vladislav_253501 <vlad.stepanov.2021@inbox.ru>
2025-02-21 13:22:13 +0300

    Creating an interface for REST clients

---
+++ TradeConnector/TradeConnector.Core/Clients/Interfaces/IRestClient.cs
@@ -0,0 +1,12 @@
+﻿using TradeConnector.Core.Models;
+
+namespace TradeConnector.Core.Clients.Interfaces.Bitfinex;
+
+public interface IRestClient
+{
+    Task<IEnumerable<Trade>> GetTradesAsync(string pair, int maxCount);
+    Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec,
+        DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0);
+    Task<PairTicker> GetTickerAsync(string pair, int maxCount);
+
+}
