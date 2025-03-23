7d386adf0d1b6b798062cb71dbbce41d08392d03
Stepanov_Vladislav_253501 <vlad.stepanov.2021@inbox.ru>
2025-03-24 02:35:03 +0300

    Creating an interface for Websocket clients

---
+++ TradeConnector/TradeConnector.Core/Clients/Interfaces/IWebSocketClient.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using TradeConnector.Core.Models;
+
+namespace TradeConnector.Core.Clients.Interfaces;
+
+public interface IWebSocketClient
+{
+    /// <summary>
+    /// For trading pairs, ex. tBTCUSD
+    /// </summary>
+    event Action<PairTrade> NewBuyPairTrade;
+    event Action<PairTrade> NewSellPairTrade;
+
+    /// <summary>
+    /// For funding currency symbols (ex. fUSD)
+    /// </summary>
+    event Action<CurrencyTrade> NewBuyCurrencyTrade;
+    event Action<CurrencyTrade> NewSellCurrencyTrade;
+
+    void SubscribePairTrades(string pair, int maxCount = 100);
+    void UnsubscribePairTrades(string pair);
+
+    void SubscribeCurrencyTrades(string currency, int maxCount = 100);
+    void UnsubscribeCurrencyTrades(string currency);
+
+    event Action<Candle> CandleSeriesProcessing;
+
+    void SubscribeCandles(string symbol, int periodInSec, DateTimeOffset? from = null, DateTimeOffset? to = null, long? count = 0);
+    void UnsubscribeCandles(string symbol);
+
+    Task ConnectAsync();
+    Task DisconnectAsync();
+}
