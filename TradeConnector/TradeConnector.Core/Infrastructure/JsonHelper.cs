ebf9696a7cd2a677c5d9753d45692a4c78a4dcf6
Stepanov_Vladislav_253501 <vlad.stepanov.2021@inbox.ru>
2025-03-24 03:31:29 +0300

    Added JsonHelper class for serialize/deserialize objects

---
+++ TradeConnector/TradeConnector.Core/Infrastructure/JsonHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json;
+
+namespace TradeConnector.Core.Infrastructure;
+
+public class JsonHelper
+{
+    public static string Serialize<T>(T obj) => JsonSerializer.Serialize(obj);
+    public static T Deserialize<T>(string json) => JsonSerializer.Deserialize<T>(json) ?? throw new Exception("Deserialization failed");
+}
