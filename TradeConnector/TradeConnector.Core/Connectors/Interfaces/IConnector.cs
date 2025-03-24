4683c18438084939343177691c4b1f058077a7e4
Stepanov_Vladislav_253501 <vlad.stepanov.2021@inbox.ru>
2025-03-24 07:16:06 +0300

    Interface update for Connector

--- TradeConnector/TradeConnector.Core/Connectors/Interfaces/IConnector.cs
+++ TradeConnector/TradeConnector.Core/Connectors/Interfaces/IConnector.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TradeConnector.Core.Models;
 
-namespace TradeConnector.Core.Connectors.Interfaces
+namespace TradeConnector.Core.Connectors.Interfaces;
+
+public interface IConnector
 {
-    interface Interface1
-    {
-    }
+    #region Rest
+
+    Task<IEnumerable<PairTrade>> GetNewPairTradesAsync(string pair, int maxCount);
+    Task<IEnumerable<CurrencyTrade>> GetNewCurrencyTradesAsync(string currency, int maxCount);
+
+    Task<IEnumerable<Candle>> GetCandleSeriesAsync(string symbol, int periodInMin,
+        DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0);
+    Task<PairTicker> GetNewPairTickerAsync(string pair);
+    Task<CurrencyTicker> GetNewCurrencyTickerAsync(string currency);
+
+    #endregion
+
+    #region Socket
+
+
+    event Action<PairTrade> NewBuyPairTrade;
+    event Action<PairTrade> NewSellPairTrade;
+    event Action<CurrencyTrade> NewBuyCurrencyTrade;
+    event Action<CurrencyTrade> NewSellCurrencyTrade;
+
+    void SubscribePairTrades(string pair, int maxCount = 100);
+    void UnsubscribePairTrades(string pair);
+
+    void SubscribeCurrencyTrades(string currency, int maxCount = 100);
+    void UnsubscribeCurrencyTrades(string currency);
+
+    event Action<Candle> CandleSeriesProcessing;
+
+    void SubscribeCandles(string symbol, int periodInSec, DateTimeOffset? from = null, DateTimeOffset? to = null, long? count = 0);
+    void UnsubscribeCandles(string symbol);
+
+    Task ConnectAsync();
+    Task DisconnectAsync();
+
+    #endregion
 }
