42cee8a54a389e8133f145a26febbac2742711a9
Stepanov_Vladislav_253501 <vlad.stepanov.2021@inbox.ru>
2025-03-24 07:34:47 +0300

    Implement balance calculation service

---
+++ TradeConnector/TradeConnector.Core/Services/PortfolioCalculator.cs
@@ -0,0 +1,47 @@
+﻿using TradeConnector.Core.Connectors.Interfaces;
+using TradeConnector.Core.Models;
+
+namespace TradeConnector.Core.Services;
+
+public class PortfolioCalculator
+{
+    private readonly IConnector _connector;
+
+    private readonly Dictionary<string, decimal> _portfolio = new()
+        {
+            {"BTC", 1},
+            {"XRP", 15000},
+            {"XMR", 50},
+            {"DASH", 30}
+        };
+
+    public PortfolioCalculator(IConnector connector)
+    {
+        _connector = connector;
+    }
+
+    public async Task<decimal> GetTotalBalanceAsync(string baseCurrency)
+    {
+        decimal totalBalance = 0;
+
+        var btcToBaseCurrency = await GetExchangeRateAsync("BTC", baseCurrency);
+        var xrpToBaseCurrency = await GetExchangeRateAsync("XRP", baseCurrency);
+        var xmrToBaseCurrency = await GetExchangeRateAsync("XMR", baseCurrency);
+        var dashToBaseCurrency = await GetExchangeRateAsync("DASH", baseCurrency);
+
+        totalBalance += _portfolio["BTC"] * btcToBaseCurrency;
+        totalBalance += _portfolio["XRP"] * xrpToBaseCurrency;
+        totalBalance += _portfolio["XMR"] * xmrToBaseCurrency;
+        totalBalance += _portfolio["DASH"] * dashToBaseCurrency;
+
+        return totalBalance;
+    }
+
+    private async Task<decimal> GetExchangeRateAsync(string fromCurrency, string toCurrency)
+    {
+        string pair = $"t{fromCurrency}{toCurrency}";
+        PairTicker ticker = await _connector.GetNewPairTickerAsync(pair);
+
+        return ticker.LastPrice;
+    }
+}
