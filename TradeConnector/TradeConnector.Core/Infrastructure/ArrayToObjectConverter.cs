67df7c1448bc7792a07354ca1e1bc3c546e1fbff
Stepanov_Vladislav_253501 <vlad.stepanov.2021@inbox.ru>
2025-03-30 06:10:28 +0300

    Implement JsonConverter that maps array elements to object properties

---
+++ TradeConnector/TradeConnector.Core/Infrastructure/ArrayToObjectConverter.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Reflection;
+using TradeConnector.Core.Models;
+
+namespace TradeConnector.Core.Infrastructure;
+
+public class ArrayToObjectConverter<T> : JsonConverter where T : new()
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(T);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        var instance = new T();
+        var properties = typeof(T).GetProperties()
+            .Select(p => new
+            {
+                Property = p,
+                Index = p.GetCustomAttribute<JsonArrayIndex>()?.Index
+            })
+            .Where(x => x.Index.HasValue)
+            .ToList();
+
+        var array = JArray.Load(reader);
+
+        foreach (var prop in properties)
+        {
+            if (prop.Index.Value < array.Count)
+            {
+                var value = array[prop.Index.Value].ToObject(prop.Property.PropertyType);
+                prop.Property.SetValue(instance, value);
+            }
+        }
+
+        return instance;
+    }
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+}
