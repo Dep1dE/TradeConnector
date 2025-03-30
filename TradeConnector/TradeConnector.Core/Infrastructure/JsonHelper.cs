7eef8f991ba24a6c1092e781454f4892b57ac74e
Stepanov_Vladislav_253501 <vlad.stepanov.2021@inbox.ru>
2025-03-30 06:13:28 +0300

    Change deserialization method to work with JsonConverter

--- TradeConnector/TradeConnector.Core/Infrastructure/JsonHelper.cs
+++ TradeConnector/TradeConnector.Core/Infrastructure/JsonHelper.cs
@@ -1,9 +1,19 @@
-﻿using System.Text.Json;
+﻿using Newtonsoft.Json;
+using System.Reflection;
+using System.Text.Json;
 
 namespace TradeConnector.Core.Infrastructure;
 
 public static class JsonHelper
 {
-    public static string Serialize<T>(T obj) => JsonSerializer.Serialize(obj);
-    public static T Deserialize<T>(string json) => JsonSerializer.Deserialize<T>(json) ?? throw new Exception("Deserialization failed");
+    public static string Serialize<T>(T obj) => JsonConvert.SerializeObject(obj);
+    public static List<T> Deserialize<T>(string json) where T : new()
+    {
+        var settings = new JsonSerializerSettings
+        {
+            Converters = { new ArrayToObjectConverter<T>() }
+        };
+
+        return JsonConvert.DeserializeObject<List<T>>(json, settings);
+    }
 }
