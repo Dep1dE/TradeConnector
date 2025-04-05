0e93263986414199caa9dbdf76204ac5d8371d8c
Stepanov_Vladislav_253501 <vlad.stepanov.2021@inbox.ru>
2025-04-05 06:17:09 +0300

    Added interface for WebSocketHelper

---
+++ TradeConnector/TradeConnector.Core/Infrastructure/Interfaces/IWebSocketHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace TradeConnector.Core.Infrastructure.Interfaces;
+
+public interface IWebSocketHelper
+{
+    event Action<string>? OnMessageReceived;
+    event Action? OnConnected;
+    event Action? OnDisconnected;
+
+    Task ConnectAsync();
+    Task DisconnectAsync();
+    Task SendMessageAsync<T>(T message);
+}
